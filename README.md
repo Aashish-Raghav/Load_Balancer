ab1fbfe70720dc59e86048440c92447aa447f792
aashishraghav627@gmail.com <aashishraghav627@gmail.com>
2024-12-25 12:03:58 +0530

    Scheduling Algorithm: Add Round Robin
    Round robin in all servers assuming all are healthy servers

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 The Challenge - Building a Load Balancer
 
-Step1: Basic routing through load balancer
+Step1: Basic routing through load balancer  
+Step2: Add scheduling algorithm (Round Robin)
--- load_balancer.py
+++ load_balancer.py
@@ -1,11 +1,12 @@
 import socket
+import argparse
+from collections import deque
 
 # define host and port
 HOST = "127.0.0.1" # local host, server listens only on the loopback interface.
 PORT_LB = 65433
-PORT_BE = 65434
 
-def load_balancer():
+def load_balancer(server_count):
     # create a socket object
     lb_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     lb_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -16,6 +17,11 @@ def load_balancer():
 
     print(f"Server started. Listening on {HOST}:{PORT_LB}...")
 
+    # having queue of servers to perform round robin
+    queue = deque([])
+    for i in range(0,server_count):
+        queue.append(i)
+    
     try:
         while True:
             # Accept a new connections
@@ -23,19 +29,50 @@ def load_balancer():
             print(f"./lb \r\nRecieved request from {client_address[0]}")
 
             # recieve data from client
-            request = client_socket.recv(1024)
+            request = client_socket.recv(4096)
             if not request:
                 break
             print((request.decode('utf-8')).strip())
-            
+
             with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as backend_server:
-                backend_server.connect((HOST,PORT_BE))
+                # Round robin algorithm
+                port = queue[0] + 8080
+                queue.append(queue[0])
+                queue.popleft()
+
+                # Create a connection to the backend server and forward request
+                backend_server.connect((HOST,port))
                 backend_server.sendall(request)
 
-                backend_response = backend_server.recv(1024)
+                # Receive response from backend
+                backend_response = backend_server.recv(4096)
                 print(f"Response from server: {backend_response.decode()}")
+                headers,_,body = backend_response.partition(b"\r\n\r\n")
 
-                client_socket.sendall(backend_response)
+                # parse content length from headers
+                headers_str = headers.decode()
+                headers_lines = headers_str.split("\r\n")
+                content_length = 0
+                for line in headers_lines:
+                    if (line.lower().startswith("content-length:")):
+                        content_length = (int) (line.split(':')[1].strip())
+                        break
+
+                # send headers to client
+                client_socket.sendall(headers + b"\r\n\r\n")
+
+                # send already received part
+                bytes_forwarded = len(body)
+                client_socket.sendall(body)
+
+                # Continue forwarding the rest of the body
+                while (bytes_forwarded < content_length):
+                    chunk = backend_server.recv(4096)
+                    if not chunk:
+                        break
+                    client_socket.sendall(chunk)
+                    bytes_forwarded += len(chunk)
+
 
             client_socket.close()
 
@@ -48,4 +85,7 @@ def load_balancer():
         lb_socket.close()
 
 if __name__ == '__main__':
-    load_balancer()
+    parser = argparse.ArgumentParser("Load Balancer")
+    parser.add_argument("-s","--server-count",default=1,type=int,help="Number of backend servers")
+    args = parser.parse_args()
+    load_balancer(args.server_count)
