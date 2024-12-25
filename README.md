226a10f79059db800a57c6c06c67ca2e1695ce3b
aashishraghav627@gmail.com <aashishraghav627@gmail.com>
2024-12-25 14:55:39 +0530

    Threading: Add Threads to check healthy servers
    Threads check healthy servers at interval, and load balancer routes
    traffic to healthy servers only

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 The Challenge - Building a Load Balancer
 
 Step1: Basic routing through load balancer  
-Step2: Add scheduling algorithm (Round Robin)
+Step2: Add scheduling algorithm (Round Robin)  
+Step3: Add Threading to check healthy servers and route traffic accordingly
--- load_balancer.py
+++ load_balancer.py
@@ -1,13 +1,19 @@
 import socket
 import argparse
 from collections import deque
+import threading
+import time
+import requests
 
 # define host and port
 HOST = "127.0.0.1"  # local host, server listens only on the loopback interface.
 PORT_LB = 65433
 
+healthy_servers = []
+servers_lock = threading.Lock()
 
-def load_balancer(server_count):
+
+def load_balancer():
     # create a socket object
     lb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     lb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -18,10 +24,8 @@ def load_balancer(server_count):
 
     print(f"Server started. Listening on {HOST}:{PORT_LB}...")
 
-    # having queue of servers to perform round robin
-    queue = deque([])
-    for i in range(0, server_count):
-        queue.append(i)
+    # having queue index of servers to perform round robin
+    curr_index = 0
 
     try:
         while True:
@@ -35,46 +39,51 @@ def load_balancer(server_count):
                 break
             print((request.decode("utf-8")).strip())
 
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as backend_server:
-                # Round robin algorithm
-                port = queue[0] + 8080
-                queue.append(queue[0])
-                queue.popleft()
+            with servers_lock:
+                with socket.socket(
+                    socket.AF_INET, socket.SOCK_STREAM
+                ) as backend_server:
+                    # Round robin algorithm
+                    server = healthy_servers[(curr_index) % len(healthy_servers)]
+                    curr_index = (curr_index + 1) % len(healthy_servers)
 
-                # Create a connection to the backend server and forward request
-                backend_server.connect((HOST, port))
-                backend_server.sendall(request)
+                    host, port = server.split(":")
+                    port = int(port)
 
-                # Receive response from backend
-                backend_response = backend_server.recv(4096)
-                print(f"Response from server: {backend_response.decode()}")
-                headers, _, body = backend_response.partition(b"\r\n\r\n")
+                    # Create a connection to the backend server and forward request
+                    backend_server.connect((host, port))
+                    backend_server.sendall(request)
 
-                # parse content length from headers
-                headers_str = headers.decode()
-                headers_lines = headers_str.split("\r\n")
-                content_length = 0
-                for line in headers_lines:
-                    if line.lower().startswith("content-length:"):
-                        content_length = (int)(line.split(":")[1].strip())
-                        break
+                    # Receive response from backend
+                    backend_response = backend_server.recv(4096)
+                    print(f"Response from server: {backend_response.decode()}")
+                    headers, _, body = backend_response.partition(b"\r\n\r\n")
 
-                # send headers to client
-                client_socket.sendall(headers + b"\r\n\r\n")
+                    # parse content length from headers
+                    headers_str = headers.decode()
+                    headers_lines = headers_str.split("\r\n")
+                    content_length = 0
+                    for line in headers_lines:
+                        if line.lower().startswith("content-length:"):
+                            content_length = (int)(line.split(":")[1].strip())
+                            break
 
-                # send already received part
-                bytes_forwarded = len(body)
-                client_socket.sendall(body)
+                    # send headers to client
+                    client_socket.sendall(headers + b"\r\n\r\n")
 
-                # Continue forwarding the rest of the body
-                while bytes_forwarded < content_length:
-                    chunk = backend_server.recv(4096)
-                    if not chunk:
-                        break
-                    client_socket.sendall(chunk)
-                    bytes_forwarded += len(chunk)
+                    # send already received part
+                    bytes_forwarded = len(body)
+                    client_socket.sendall(body)
 
-            client_socket.close()
+                    # Continue forwarding the rest of the body
+                    while bytes_forwarded < content_length:
+                        chunk = backend_server.recv(4096)
+                        if not chunk:
+                            break
+                        client_socket.sendall(chunk)
+                        bytes_forwarded += len(chunk)
+
+                client_socket.close()
 
     except KeyboardInterrupt:
         print("\load_balancer is shutting down.")
@@ -84,10 +93,50 @@ def load_balancer(server_count):
         lb_socket.close()
 
 
+def add_protocol(server):
+    if not server.startswith("http://") or not server.startwith("https://"):
+        server = "http://" + server
+    return server
+
+
+def server_health_check(servers, interval):
+    global healthy_servers
+    while True:
+        temporary_healthy_servers = []
+        for server in servers:
+            try:
+                response = requests.get(add_protocol(server), timeout=2)
+                print(response)
+                if response.status_code == 200:
+                    temporary_healthy_servers.append(server)
+            except requests.exceptions.RequestException:
+                print(f"{server} is unreachable")
+
+        with servers_lock:
+            healthy_servers = temporary_healthy_servers
+
+        time.sleep(interval)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Load Balancer")
     parser.add_argument(
-        "-s", "--server-count", default=1, type=int, help="Number of backend servers"
+        "-s", "--servers", nargs="+", required=True, help="backend servers"
+    )
+    parser.add_argument(
+        "-t",
+        "--interval",
+        default=5,
+        type=int,
+        help="interval in seconds after which check servers health",
     )
     args = parser.parse_args()
-    load_balancer(args.server_count)
+
+    # Start the health check thread
+    health_check_thread = threading.Thread(
+        target=server_health_check, args=(args.servers, args.interval), daemon=True
+    )
+    health_check_thread.start()
+
+    # Start load balancer
+    load_balancer()
