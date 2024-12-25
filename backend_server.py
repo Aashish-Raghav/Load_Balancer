e9f8e1a2918df4613ce1265c3192113f5720be21
aashishraghav627@gmail.com <aashishraghav627@gmail.com>
2024-12-25 12:09:59 +0530

    Style Check with black

--- backend_server.py
+++ backend_server.py
@@ -2,21 +2,22 @@ import socket
 
 
 # define host and port
-HOST = "127.0.0.1" # local host, server listens only on the loopback interface.
+HOST = "127.0.0.1"  # local host, server listens only on the loopback interface.
 PORT_BE = 65434
 
-def backend_server():
-    be_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    be_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 
-    be_socket.bind((HOST,PORT_BE))
+def backend_server():
+    be_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    be_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    be_socket.bind((HOST, PORT_BE))
     be_socket.listen(5)
 
     print(f"Server started. Listening on {HOST}:{PORT_BE}...")
 
     try:
         while True:
-            lb_socket,lb_address = be_socket.accept()
+            lb_socket, lb_address = be_socket.accept()
             print(f"./be \r\n Received request from {lb_address[0]}")
 
             # receive data from load_balancer
@@ -24,7 +25,7 @@ def backend_server():
             if not request:
                 break
             print(request.decode())
-            
+
             # Create a valid HTTP response
             response_body = "Hello From Backend Server"
             response_headers = (
@@ -45,7 +46,7 @@ def backend_server():
     finally:
         # Close the server socket
         be_socket.close()
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     backend_server()
--- load_balancer.py
+++ load_balancer.py
@@ -3,59 +3,60 @@ import argparse
 from collections import deque
 
 # define host and port
-HOST = "127.0.0.1" # local host, server listens only on the loopback interface.
+HOST = "127.0.0.1"  # local host, server listens only on the loopback interface.
 PORT_LB = 65433
+
 
 def load_balancer(server_count):
     # create a socket object
-    lb_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    lb_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    lb_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    lb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    lb_socket.bind((HOST,PORT_LB))
+    lb_socket.bind((HOST, PORT_LB))
 
-    lb_socket.listen(5) # maximum number of queued connections
+    lb_socket.listen(5)  # maximum number of queued connections
 
     print(f"Server started. Listening on {HOST}:{PORT_LB}...")
 
     # having queue of servers to perform round robin
     queue = deque([])
-    for i in range(0,server_count):
+    for i in range(0, server_count):
         queue.append(i)
-    
+
     try:
         while True:
             # Accept a new connections
-            client_socket,client_address = lb_socket.accept()
+            client_socket, client_address = lb_socket.accept()
             print(f"./lb \r\nRecieved request from {client_address[0]}")
 
             # recieve data from client
             request = client_socket.recv(4096)
             if not request:
                 break
-            print((request.decode('utf-8')).strip())
+            print((request.decode("utf-8")).strip())
 
-            with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as backend_server:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as backend_server:
                 # Round robin algorithm
                 port = queue[0] + 8080
                 queue.append(queue[0])
                 queue.popleft()
 
                 # Create a connection to the backend server and forward request
-                backend_server.connect((HOST,port))
+                backend_server.connect((HOST, port))
                 backend_server.sendall(request)
 
                 # Receive response from backend
                 backend_response = backend_server.recv(4096)
                 print(f"Response from server: {backend_response.decode()}")
-                headers,_,body = backend_response.partition(b"\r\n\r\n")
+                headers, _, body = backend_response.partition(b"\r\n\r\n")
 
                 # parse content length from headers
                 headers_str = headers.decode()
                 headers_lines = headers_str.split("\r\n")
                 content_length = 0
                 for line in headers_lines:
-                    if (line.lower().startswith("content-length:")):
-                        content_length = (int) (line.split(':')[1].strip())
+                    if line.lower().startswith("content-length:"):
+                        content_length = (int)(line.split(":")[1].strip())
                         break
 
                 # send headers to client
@@ -66,16 +67,14 @@ def load_balancer(server_count):
                 client_socket.sendall(body)
 
                 # Continue forwarding the rest of the body
-                while (bytes_forwarded < content_length):
+                while bytes_forwarded < content_length:
                     chunk = backend_server.recv(4096)
                     if not chunk:
                         break
                     client_socket.sendall(chunk)
                     bytes_forwarded += len(chunk)
 
-
             client_socket.close()
-
 
     except KeyboardInterrupt:
         print("\load_balancer is shutting down.")
@@ -84,8 +83,11 @@ def load_balancer(server_count):
         # Close the server socket
         lb_socket.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser("Load Balancer")
-    parser.add_argument("-s","--server-count",default=1,type=int,help="Number of backend servers")
+    parser.add_argument(
+        "-s", "--server-count", default=1, type=int, help="Number of backend servers"
+    )
     args = parser.parse_args()
     load_balancer(args.server_count)
